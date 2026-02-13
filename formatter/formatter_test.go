1ef3750d6c262fe78d3a41f354c3e9016d51436f
John Bartholomew <jpa.bartholomew@gmail.com>
2026-02-13 19:48:40 +0000

    simplify and clean up formatter_test
    There were various bits of dead code and easily simplified things.

--- formatter/formatter_test.go
+++ formatter/formatter_test.go
@@ -3,8 +3,7 @@ package formatter
 import (
 	"flag"
 	"fmt"
-	"io"
-	"io/ioutil"
+	"os"
 	"path/filepath"
 	"regexp"
 	"strings"
@@ -15,69 +14,26 @@ import (
 
 var update = flag.Bool("update", false, "update .golden files")
 
-// ErrorWriter encapsulates a writer and an error state indicating when at least
-// one error has been written to the writer.
-type ErrorWriter struct {
-	ErrorsFound bool
-	Writer      io.Writer
-}
-
 type formatterTest struct {
-	name   string
-	input  string
-	output string
+	name       string
+	input      string
+	goldenPath string
 }
 
-type ChangedGoldensList struct {
-	changedGoldens []string
-}
-
-func runTest(t *testing.T, test *formatterTest, changedGoldensList *ChangedGoldensList) {
-	read := func(file string) []byte {
-		bytz, err := ioutil.ReadFile(file)
-		if err != nil {
-			t.Fatalf("reading file: %s: %v", file, err)
-		}
-		return bytz
-	}
-
-	input := read(test.input)
-	var outBuilder strings.Builder
-	output, err := Format(test.name, string(input), DefaultOptions())
+func mustReadFile(t *testing.T, file string) []byte {
+	t.Helper()
+	bytz, err := os.ReadFile(file)
 	if err != nil {
-		errWriter := ErrorWriter{
-			Writer:      &outBuilder,
-			ErrorsFound: false,
-		}
-
-		_, writeErr := errWriter.Writer.Write([]byte(err.Error()))
-		if writeErr != nil {
-			panic(writeErr)
-		}
-	} else {
-		outBuilder.Write([]byte(output))
+		t.Fatalf("reading file: %s: %v", file, err)
 	}
+	return bytz
+}
 
-	outData := outBuilder.String()
-
-	if *update {
-		changed, err := testutils.UpdateGoldenFile(test.output, []byte(outData), 0666)
-		if err != nil {
-			t.Error(err)
-		}
-		if changed {
-			changedGoldensList.changedGoldens = append(changedGoldensList.changedGoldens, test.output)
-		}
-	} else {
-		golden, err := ioutil.ReadFile(test.output)
-		if err != nil {
-			t.Error(err)
-			return
-		}
-		if diff, hasDiff := testutils.CompareWithGolden(outData, golden); hasDiff {
-			t.Error(fmt.Errorf("golden file %v has diff:\n%v", test.input, diff))
-		}
+func coalesceError(out string, err error) (string, bool) {
+	if err != nil {
+		return err.Error(), false
 	}
+	return out, true
 }
 
 func TestFormatter(t *testing.T) {
@@ -100,16 +56,30 @@ func TestFormatter(t *testing.T) {
 		name := jsonnetExtRE.ReplaceAllString(input, "")
 		golden := jsonnetExtRE.ReplaceAllString(input, ".fmt.golden")
 		tests = append(tests, &formatterTest{
-			name:   name,
-			input:  input,
-			output: golden,
+			name:       name,
+			input:      string(mustReadFile(t, input)),
+			goldenPath: golden,
 		})
 	}
 
-	changedGoldensList := ChangedGoldensList{}
+	var changedGoldens []string
 	for _, test := range tests {
 		t.Run(test.name, func(t *testing.T) {
-			runTest(t, test, &changedGoldensList)
+			out, _ := coalesceError(Format(test.name, test.input, DefaultOptions()))
+			if *update {
+				changed, err := testutils.UpdateGoldenFile(test.goldenPath, []byte(out), 0666)
+				if err != nil {
+					t.Error(err)
+				}
+				if changed {
+					changedGoldens = append(changedGoldens, test.goldenPath)
+				}
+			} else {
+				golden := mustReadFile(t, test.goldenPath)
+				if diff, hasDiff := testutils.CompareWithGolden(out, golden); hasDiff {
+					t.Error(fmt.Errorf("golden file for %v has diff:\n%v", test.name, diff))
+				}
+			}
 		})
 	}
 
@@ -117,8 +87,8 @@ func TestFormatter(t *testing.T) {
 		// Little hack: a failed test which prints update stats.
 		t.Run("Goldens Updated", func(t *testing.T) {
 			t.Logf("Expected failure, for printing update stats. Does not appear without `-update`.")
-			t.Logf("%d formatter goldens updated:\n", len(changedGoldensList.changedGoldens))
-			for _, golden := range changedGoldensList.changedGoldens {
+			t.Logf("%d formatter goldens updated:\n", len(changedGoldens))
+			for _, golden := range changedGoldens {
 				t.Log(golden)
 			}
 			t.Fail()
